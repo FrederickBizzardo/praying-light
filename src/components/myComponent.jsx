761ad9f4e679c0b47a5fa965ca6c17215fdabb43
FrederickBizzardo <nyembofred@gmail.com>
2024-02-26 13:47:29 +0000

    API Operational

--- src/components/myComponent.jsx
+++ src/components/myComponent.jsx
@@ -3,14 +3,28 @@ import React, { useState, useEffect } from 'react'
  const response = await fetch('http://calapi.inadiutorium.cz/api/v0/en/calendars/default/today')
  
  const data = await response.json();
-const randomUser = data.celebrations[0];
-    
+const day = data.celebrations[0];
+// const date = data.celebrations[0];
+
+const index = day.title;
 
 console.log(data);
 
 function Movies() {
 // Output the result to the page
-  return <div>{JSON.stringify(data)}</div>;
+
+/* return <div>{JSON.stringify(data)}</div>;
+*/
+
+/* return <div>{JSON.stringify(date)}</div>;
+*/
+
+return <div>
+  {JSON.stringify(index)}
+</div>;
+
+  
 };
 
   export default Movies;
+  
